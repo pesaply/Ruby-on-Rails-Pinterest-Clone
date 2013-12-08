cc79ce3c722ff7ba9debd3a240ce97d1124ff0fa
Cody H <10halec@gmail.com>
2013-12-08 15:43:18 -0700

    add jQuery masonry

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -5,7 +5,7 @@ class PinsController < ApplicationController
   # GET /pins
   # GET /pins.json
   def index
-    @pins = Pin.all
+    @pins = Pin.order("created_at desc")
   end
 
   # GET /pins/1
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,6 @@
 class Pin < ActiveRecord::Base
 
-has_attached_file :image, styles: { medium: "300x300>" }
+has_attached_file :image, styles: { medium: "320x500>" }
 
 
 validates :description, presence: true
